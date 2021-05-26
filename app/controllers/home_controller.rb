a69300dc2861c37b4692172cc5c0b00c97034a36
Lam Tran <s3714110@student.rmit.edu.au>
2021-05-26 19:09:05 +0000

    moved newsletter to partial

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,12 +7,18 @@ class HomeController < ApplicationController
   end
 
   def send_email
-     @email_visitor = params[:visitor_email]
+    @email_visitor = params[:visitor_email]
     unless @email_visitor.blank?
       UserNotifierMailer.send_news_email(@email_visitor).deliver
-      redirect_to root_path
+
     end
+
+    respond_to do |format|
+      format.js
+    end
+
   end
+
 
   def reset_product
     @random_product = Product.find(Product.pluck(:id).sample)
