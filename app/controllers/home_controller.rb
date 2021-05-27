e17a102ade573b60855a516fa1d62f7c8d6b9077
Lam Tran <s3714110@student.rmit.edu.au>
2021-05-27 19:19:34 +0000

    added saved list cookies, add icon now changes to minus if product is already added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,9 @@ class HomeController < ApplicationController
   def index
     @random_product = Product.find(Product.pluck(:id).sample)
     @popular_products = Product.order('popularity DESC, name')
+    if !cookies[:saved].present?
+      cookies.permanent[:saved] = JSON.generate([])
+    end
   end
 
   def help
@@ -23,11 +26,25 @@ class HomeController < ApplicationController
 
 
   def reset_product
+    if !cookies[:saved].present?
+      cookies.permanent[:saved] = JSON.generate([])
+    end
+
     @current_random_product = Product.find_by_name(params[:random_product_name])
     @current_random_product.popularity += 1
     if @current_random_product.valid?
       @current_random_product.save!
     end
+
+    @current_saved_list = JSON.parse(cookies[:saved])
+
+    if @current_saved_list.include?(@current_random_product.name)
+      @current_saved_list.delete(@current_random_product.name)
+    else
+      @current_saved_list.push(@current_random_product.name)
+    end
+
+    cookies.permanent[:saved] = JSON.generate(@current_saved_list)
 
     @random_product = Product.find(Product.pluck(:id).sample)
 
@@ -38,11 +55,32 @@ class HomeController < ApplicationController
   end
 
   def add_to_list
+    if !cookies[:saved].present?
+      cookies.permanent[:saved] = JSON.generate([])
+    end
+
     @current_product = Product.find_by_name(params[:current_product_name])
     @current_product.popularity += 1
     if @current_product.valid?
       @current_product.save!
     end
+
+    @current_saved_list = JSON.parse(cookies[:saved])
+
+    if @current_saved_list.include?(@current_product.name)
+      @current_saved_list.delete(@current_product.name)
+    else
+      @current_saved_list.push(@current_product.name)
+    end
+
+    cookies.permanent[:saved] = JSON.generate(@current_saved_list)
+
+    @popular_products = Product.order('popularity DESC, name')
+
+    respond_to do |format|
+      format.js
+    end
+
   end
 
 end
