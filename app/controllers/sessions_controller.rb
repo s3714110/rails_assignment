2df3c38b042f2123907201a114b671df3a79bd0b
Lam Tran <s3714110@student.rmit.edu.au>
2021-05-28 15:41:40 +0000

    edit page and permissions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:session][:password])
       log_in user
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-      redirect_to root_path
+      redirect_back_or(user)
     else
       flash.now[:danger] = 'Wrong email or password. Try again!'
       render 'new'
